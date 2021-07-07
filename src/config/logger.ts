fd07948f6c3252e667ee256ec766384f556188c1
Pablo Danilo <pablodanilo75321@gmail.com>
2021-07-06 21:16:17 -0300

    feat: created log config

---
+++ src/config/logger.ts
@@ -0,0 +1,21 @@
+import { format } from 'winston'
+
+import { isTestEnvironment } from '~/common/conditions'
+
+/**
+ * Logging level.
+ */
+const INFO_LOG = 'info'
+
+const customFormat = format.printf(info => {
+  return `${info.level} - ${info.message}`
+})
+
+/**
+ * Console options to dev logs.
+ */
+export const consoleOptions = {
+  level: INFO_LOG,
+  format: customFormat,
+  silent: isTestEnvironment
+}

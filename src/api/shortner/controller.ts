398e21a14b69ec2c28b099bd96e7374feb8b20ff
Pablo Danilo <pablodanilo75321@gmail.com>
2021-07-08 22:58:16 -0300

    feat: added service to get a shortened url

--- src/api/shortner/controller.ts
+++ src/api/shortner/controller.ts
@@ -1,6 +1,14 @@
-import { Body, Post, OnUndefined, JsonController } from 'routing-controllers'
+import {
+  Get,
+  Body,
+  Post,
+  Params,
+  OnUndefined,
+  JsonController,
+  Res
+} from 'routing-controllers'
 
-import { save } from './service'
+import { findOne, save } from './service'
 import { ShortnerRequest } from './requests/shortner-request'
 
 @JsonController('/shortner')
@@ -8,6 +16,25 @@ export class ShortnerController {
   @Post('/')
   @OnUndefined(204)
   async save(@Body() shortnerRequest: ShortnerRequest) {
+    /**
+     * Save url.
+     */
     return save(shortnerRequest)
+  }
+
+  @Get('/:hash')
+  @OnUndefined(204)
+  async getUrl(@Params() filter: { hash: string }, @Res() response: any) {
+    try {
+      /**
+       * Get url.
+       */
+      const foundUrl = await findOne(filter)
+
+      response.setHeader('Content-Type', 'json/application')
+      return await response.redirect(foundUrl)
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
--- src/api/shortner/service.ts
+++ src/api/shortner/service.ts
@@ -1,7 +1,9 @@
-import { Url } from '~/models/url'
+import { FilterQuery } from 'mongoose'
+
 import { logger } from '~/common/logger'
 import { objectToJSON } from '~/common/object'
 import { ShortnerRequest } from './requests/shortner-request'
+import { Url, UrlDocument } from '~/models/url'
 
 const createHash = require('hash-generator')
 
@@ -44,4 +46,26 @@ export const save = async (shortnerRequest: ShortnerRequest) => {
   logger.info(`saved url: ${objectToJSON(savedUrl)}`)
 
   return savedUrl.hash
+}
+
+/**
+ * Find one url by filter.
+ */
+export const findOne = async (filter: FilterQuery<UrlDocument>) => {
+  /**
+   * Log.
+   */
+  logger.info(`Buscando url através do filtro: ${objectToJSON(filter)}`)
+
+  /**
+   * Find url.
+   */
+  const foundUrl = await Url.findOne(filter).lean<UrlDocument>()
+
+  /**
+   * Log.
+   */
+  logger.info(`Url encontrada: ${objectToJSON(foundUrl)}`)
+
+  return foundUrl.url
 }
